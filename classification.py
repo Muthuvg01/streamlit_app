91aaf6226c712a0e374b3f61c032142c054441a2
Muthuvg01 <157470480+Muthuvg01@users.noreply.github.com>
2024-02-20 15:19:55 +0530

    classification.py

--- classification.py
+++ classification.py
@@ -12,7 +12,7 @@ from sklearn.preprocessing import MinMaxScaler
 from imblearn.over_sampling import SMOTE
 import pickle
 
-st.title("Classifying Bookings")
+st.title("Bookings classification")
 with open("knn.pkl", 'rb') as file:
     rfc = pickle.load(file)
 
